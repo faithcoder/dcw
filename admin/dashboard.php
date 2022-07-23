f327e9fa1c20b88f920a5ede2c62f94d1b0cc35f
faithcoder <48954386+faithcoder@users.noreply.github.com>
2022-07-23 21:54:20 +0600

    font-awesome js files deleted

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -51,10 +51,10 @@ $auth->isLogin() ? false : header("location: login.php");
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                        Hi
+                        Hi <?= $_SESSION['user_name'];?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Profile</a>
                         <a class="dropdown-item" href="logout.php">Logout</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Something else here</a>
